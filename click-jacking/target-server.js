f327b587ac63bd4f91538121d891569d0b5d5b70
ixio0330 <ixio0330@gmail.com>
2026-02-12 18:03:28 +0900

    클릭재킹 실습

--- click-jacking/target-server.js
+++ click-jacking/target-server.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const app = express();
-const PORT = 3000;
+const PORT = 3001;
 
-app.use(express.static("public"));
+app.use(
+  express.static("public", {
+    setHeaders: (res, path, stat) => {
+      res.header("X-Frame-Options", "SAMEORIGIN");
+    },
+  }),
+);
 
 app.set("view engine", "ejs");
 
