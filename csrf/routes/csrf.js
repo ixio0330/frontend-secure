a5de994ba9ef1ff727e99673198d3698b9353a45
ixio0330 <ixio0330@gmail.com>
2026-02-12 17:38:45 +0900

    CSRF 실습 세팅

---
+++ csrf/routes/csrf.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
+const router = express.Router();
+
+router.use(
+  session({
+    secret: "session",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+      maxAge: 60 * 1000 * 5,
+    },
+  }),
+);
+
+router.use(express.urlencoded({ extended: true }));
+router.use(cookieParser());
+
+let sessionData = {};
+
+router.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  if (username !== "test" || password !== "1234") {
+    res.status(403);
+    res.send("로그인 실패");
+    return;
+  }
+
+  sessionData = req.session;
+  sessionData.username = username;
+  res.redirect("/send.html");
+});
+
+router.post("/send", (req, res) => {
+  if (
+    req.session.username !== "test" ||
+    req.session.username !== sessionData.username
+  ) {
+    res.status(403);
+    res.send("로그인이 필요합니다");
+    return;
+  }
+
+  const { to, amount } = req.body;
+  res.send(`${to}에게 ${amount}원을 송금하였습니다.`);
+});
+
+module.exports = router;
--- csrf/server.js
+++ csrf/server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
-const crypto = require("crypto");
+const csrf = require("./routes/csrf");
 
-app.use(express.static("."));
+app.use(express.static("public"));
 
 app.set("view engine", "ejs");
+
+app.use(csrf);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
